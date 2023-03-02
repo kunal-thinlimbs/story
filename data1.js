4b923783f5cfb15b7c97a32c483bf1474342b123
Kunal Patley <kunal.patley@hdworks.in>
2023-03-02 20:00:00 +0530

    sum of two

--- data1.js
+++ data1.js
@@ -16,7 +16,7 @@ const getRecentStories= [
     
     {
       "_id": "5e84765d1fec5e000721d92c",
-      "heading": "new heading",
+      "heading": "new heding",
       "images": [
         {
           "id": "1585739039893-0",
